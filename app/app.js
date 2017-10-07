3eb7812dfbaa891a0b6f6daf10eef0b1f700989b
NancyBabich <maciej.m.maslowski@gmail.com>
2017-10-07 11:11:20 +0200

    restructure repo and update prettier script

---
+++ app/app.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const hapi = require('hapi');
+const inert = require('inert');
+const products = require('./public/api/products.json');
+
+const server = new hapi.Server();
+server.connection({ port: 3000, host: 'localhost' });
+
+server.register(inert, err => {
+  server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: path.join(__dirname, 'public'),
+        listing: true
+      }
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/api/products',
+    handler(request, reply) {
+      reply(products);
+    }
+  });
+
+  server.start(err => {
+    console.log(`Server running at: ${server.info.uri}`);
+  });
+});
